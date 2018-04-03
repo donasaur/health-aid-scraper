f3efa691e1ab2e4cd1a4a26e1c0fe9822763332a
donasaur <donhmai@gmail.com>
2018-04-03 01:18:43 -0700

    added code to generate simplified newspapers view

--- process-csv-entries.js
+++ process-csv-entries.js
@@ -1,7 +1,11 @@
 
 var fs = require('fs');
-var parse = require('csv-parse/lib/sync');
 const { URL } = require('url');
+var _ = require('lodash');
+var parse = require('csv-parse/lib/sync');
+var Json2csvParser = require('json2csv').Parser;
+
+var GEN_SIMPLIFIED_NEWSPAPERS_VIEW = true;
 
 var origNewspapersCsv = fs.readFileSync('./newspapers.csv', 'utf8');
 
@@ -44,3 +48,22 @@ searchTerms.forEach(function(term) {
 console.log(searchTerms[23]);
 
 fs.writeFileSync('./search-terms.json', JSON.stringify(searchTerms), 'utf8');
+
+if (GEN_SIMPLIFIED_NEWSPAPERS_VIEW) {
+  var csvFields = ['hostname', 'world region', 'country', 'iso code', 'rank', 'newspaper title', 'language'];
+  var objFields = ['uid', 'worldRegion', 'country', 'isoCode', 'rank', 'title', 'language'];
+
+  var fieldMappingsList = [];
+  csvFields.forEach(function(e, i) {
+    fieldMappingsList.push({
+      label: csvFields[i],
+      value: objFields[i]
+    });
+  });
+
+  var sortedEntries = _.sortBy(newspapers, ['country', 'isoCode', 'rank', 'newspaperTitle', 'uid', 'name']);
+
+  var outputView = (new Json2csvParser({ fields: fieldMappingsList, delimiter: '\t' })).parse(sortedEntries);
+
+  fs.writeFileSync('./simplified-newspapers-view.txt', outputView, 'utf8');
+}
