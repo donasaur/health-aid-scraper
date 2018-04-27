76a32c0dbbe8e1dad2c8c7073b702d82dd1969a5
donasaur <donhmai@gmail.com>
2018-04-26 21:25:06 -0700

    add code to make matching more robust

--- generate-matching-data-view.js
+++ generate-matching-data-view.js
@@ -5,20 +5,27 @@ var _ = require('lodash');
 var parse = require('csv-parse/lib/sync');
 var Json2csvParser = require('json2csv').Parser;
 var webpagesCsv = fs.readFileSync('./webpages-data-view-deduped.txt', 'utf8');
+var Entities = require('html-entities').AllHtmlEntities;
 
 var WEBPAGES_PREFIX = './webpages';
 
-var PEPFAR_TERMS = ["PEPFAR", "President's Emergency Plan for AIDS Relief", "Presidential Emergency Plan for AIDS Relief", "Presidents Emergency Plan for AIDS Relief", "Plano de Emergência do Presidente dos Estados Unidos para o Alívio da SIDA", "Plano de Emergência do Presidente dos E.U.A. para o Alívio do SIDA", "Plano de Emergência do Presidente dos Estados", "Plano de Emergência do Presidente para o Alívio do VIH/SIDA ", "Plano de Emergência do Presidente dos Estados Unidos para Alívio da Sida", "Plano de Emergência do Presidente dos EUA para o Alívio do SIDA", "Plan d'urgence du Président pour la lutte contre le SIDA", "Plan Présidentiel d'Urgence contre le SIDA", "Plan d'Urgence du Président Américain pour la Lutte contre le SIDA ", "Plan d’aide d’Urgence du Président à la Lutte Contre le SIDA", "Plan d'urgence du Président des États-Unis pour la lutte contre le sida", "du Plan présidentiel américain d' aide d'urgence à la lutte contre le sida", "Plan de Emergencia del Presidente de los Estados Unidos para el Alivio del Sida", "Plan Presidencial de Emergencia para Alivio del SIDA ", "Plan de Emergencia del Presidente para el Alivio del SIDA ", "Plan de Emergencia del Presidente de los E.U. para el Alivio del Sida", "Plan de Emergencia para el Alivio del SIDA del Presidente de los Estados Unidos", "美国总统紧急救援计划。"].map(e => {
-  return e.toLowerCase().trim();
+var alphanumericRegex = /[^0-9a-z]/gi;
+var entities = new Entities();
+
+var PEPFAR_TERMS = ["PEPFAR", "President's Emergency Plan for AIDS Relief", "Presidential Emergency Plan for AIDS Relief", "Presidents Emergency Plan for AIDS Relief", "Plano de Emergência do Presidente dos Estados Unidos para o Alívio da SIDA", "Plano de Emergência do Presidente dos E.U.A. para o Alívio do SIDA", "Plano de Emergência do Presidente dos Estados", "Plano de Emergência do Presidente para o Alívio do VIH/SIDA ", "Plano de Emergência do Presidente dos Estados Unidos para Alívio da Sida", "Plano de Emergência do Presidente dos EUA para o Alívio do SIDA", "Plan d'urgence du Président pour la lutte contre le SIDA", "Plan Présidentiel d'Urgence contre le SIDA", "Plan d'Urgence du Président Américain pour la Lutte contre le SIDA ", "Plan d’aide d’Urgence du Président à la Lutte Contre le SIDA", "Plan d'urgence du Président des États-Unis pour la lutte contre le sida", "du Plan présidentiel américain d' aide d'urgence à la lutte contre le sida", "Plan de Emergencia del Presidente de los Estados Unidos para el Alivio del Sida", "Plan Presidencial de Emergencia para Alivio del SIDA ", "Plan de Emergencia del Presidente para el Alivio del SIDA ", "Plan de Emergencia del Presidente de los E.U. para el Alivio del Sida", "Plan de Emergencia para el Alivio del SIDA del Presidente de los Estados Unidos"].map(e => {
+  return e.toLowerCase().replace(alphanumericRegex, '');
 });
 
 var PMI_OR_TERMS = ["malaria", "paludisme", "malária"].map(e => {
-  return e.toLowerCase().trim();
+  return e.toLowerCase().replace(alphanumericRegex, '');
 });
 
-var PMI_TERMS = ["President's Malaria Initiative", "Presidents Malaria Initiative", "Presidential Malaria Initiative", "Iniciativa Presidencial Contra a Malaria", "Iniciativa do Presidente dos Estados Unidos para o Controlo da Malária", "Iniciativa Presidencial de Luta contra a Malária", "Iniciativa Presidencial Contra a Malária", "Initiative Malaria du Président des États-Unis ", "Initiative Présidentielle Contre le Paludisme", "Initiative du Président des États-Unis pour la lutte contre le paludisme", "Président des États-Unis de l'Initiative pour lutter contre le paludisme", "Iniciativa del Presidente contra la Malaria", "Iniciativa Presidencial Americana contra a Malária", "美国总统防治疟疾行动计划 "].map(e => {
-  return e.toLowerCase().trim();
+var PMI_TERMS = ["President's Malaria Initiative", "Presidents Malaria Initiative", "Presidential Malaria Initiative", "Iniciativa Presidencial Contra a Malaria", "Iniciativa do Presidente dos Estados Unidos para o Controlo da Malária", "Iniciativa Presidencial de Luta contra a Malária", "Iniciativa Presidencial Contra a Malária", "Initiative Malaria du Président des États-Unis ", "Initiative Présidentielle Contre le Paludisme", "Initiative du Président des États-Unis pour la lutte contre le paludisme", "Président des États-Unis de l'Initiative pour lutter contre le paludisme", "Iniciativa del Presidente contra la Malaria", "Iniciativa Presidencial Americana contra a Malária"].map(e => {
+  return e.toLowerCase().replace(alphanumericRegex, '');
 });
+
+SPECIAL_PEPFAR_TERMS = ["总统防治艾滋病紧急救援计划"];
+SPECIAL_PMI_TERMS = ["总统防治疟疾行动计划"];
 
 var webpages = parse(webpagesCsv, {
   columns: true,
@@ -40,12 +47,25 @@ webpages.forEach(webpage => {
   webpage['pepfar term(s) present?'] = false;
   webpage['pmi term(s) present?'] = false;
 
-  var webpageContents = fs.readFileSync(path.join(WEBPAGES_PREFIX, webpage.fileId + '.output'), 'utf8').toLowerCase();
+  var webpageContents = entities.decode(fs.readFileSync(path.join(WEBPAGES_PREFIX, webpage.fileId + '.output'), 'utf8'));
+
+  var specialWebpageContents = _.cloneDeep(webpageContents);
+  webpageContents = webpageContents.toLowerCase().replace(alphanumericRegex, '');
 
   for (var i = 0; i < PEPFAR_TERMS.length; i++) {
     if (webpageContents.includes(PEPFAR_TERMS[i])) {
       webpage['pepfar term(s) present?'] = true;
       break;
+    }
+  }
+
+  if (webpage.language === 'Mandarin') {
+    console.log('Mandarin page');
+    for (var is = 0; is < SPECIAL_PEPFAR_TERMS.length; is++) {
+      if (specialWebpageContents.includes(SPECIAL_PEPFAR_TERMS[is])) {
+        webpage['pepfar term(s) present?'] = true;
+        break;
+      }
     }
   }
 
@@ -63,6 +83,16 @@ webpages.forEach(webpage => {
       if (webpageContents.includes(PMI_TERMS[k])) {
         webpage['pmi term(s) present?'] = true;
         break;
+      }
+    }
+
+    if (webpage.language === 'Mandarin') {
+      console.log('Mandarin page');
+      for (var ks = 0; ks < SPECIAL_PMI_TERMS.length; ks++) {
+        if (specialWebpageContents.includes(SPECIAL_PMI_TERMS[ks])) {
+          webpage['pmi term(s) present?'] = true;
+          break;
+        }
       }
     }
   }
